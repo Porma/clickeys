cdd3ffa364182223c044fe8d583bd1b25ac2ab00
Pramodh <pormahd@gmail.com>
2021-04-14 01:24:14 +1000

    got rid of the brrrr
    but the second keystroke lackin the clackin

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import keyboard
 import random
 from playsound import playsound
 
+# used to track keypress states
+key_state = {}
+
 keypress_special_sounds = {
     'space': ['sounds/SpaceBar1.mp3', 'sounds/SpaceBar2.mp3'],
     'backspace': ['sounds/Backspace_Enter1.mp3', 'sounds/Backspace_Enter2.mp3'],
@@ -16,25 +19,35 @@ keypress_generic_sounds = [
 
 
 def main():
-    print('ClicKeys clackering');
+    print('ClicKeys clackering')
 
-    # listen for key press
-    keyboard.on_press(key_pressed);
-    # blocking listener for endless loop kinda thing
-    keyboard.wait();
+    while True:
+        key = keyboard.read_event()
+        # print('{0}: {1}'.format(key.event_type, key.name))
+        key_pressed(key)
 
 
 def key_pressed(key):
-    print('key pressed: {0}'.format(key.name));
+    print('{0}: {1}'.format(key.event_type, key.name))
 
-    # if special key was press
-    if key.name in keypress_special_sounds:
-        # play random sound from key's sound list
-        playsound(random.choice(keypress_special_sounds[key.name]), False)
-    else:
-        # else play generic keystroke sound
-        playsound(random.choice(keypress_generic_sounds), False);
+    # on keypress if press state for key is false or it does not exist in state dict
+    if key.event_type == 'down' and (key.name not in key_state or not key_state[key.name]):
+        # if special key was pressed
+        if key.name in keypress_special_sounds:
+            # play random sound from key's sound list
+            playsound(random.choice(keypress_special_sounds[key.name]), False)
+        else:
+            # else play generic keystroke sound
+            playsound(random.choice(keypress_generic_sounds), False);
+
+        # set key state to pressed
+        key_state[key.name] = True
+
+    # set key state to released on key up
+    if key.event_type == 'up':
+        key_state[key.name] = False
 
 
 if __name__ == '__main__':
-    main();
+    main()
+
