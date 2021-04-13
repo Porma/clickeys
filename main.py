448ae6ab3c95007e22efce6b0d51403f01980d2b
Pramodh <pormahd@gmail.com>
2021-04-14 00:09:51 +1000

    added special sounds for spacebar, backspace, enter

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import keyboard
 import random
 from playsound import playsound
 
+keypress_special_sounds = {
+    'space': ['sounds/SpaceBar1.mp3', 'sounds/SpaceBar2.mp3'],
+    'backspace': ['sounds/Backspace_Enter1.mp3', 'sounds/Backspace_Enter2.mp3'],
+    'enter': ['sounds/Backspace_Enter1.mp3', 'sounds/Backspace_Enter2.mp3']
+}
+
 keypress_generic_sounds = [
     'sounds/Keystroke1.mp3',
     'sounds/Keystroke2.mp3',
@@ -12,16 +18,23 @@ keypress_generic_sounds = [
 def main():
     print('ClicKeys clackering');
 
+    # listen for key press
     keyboard.on_press(key_pressed);
+    # blocking listener for endless loop kinda thing
     keyboard.wait();
 
 
 def key_pressed(key):
     print('key pressed: {0}'.format(key.name));
-    playsound(random.choice(keypress_generic_sounds), False);
+
+    # if special key was press
+    if key.name in keypress_special_sounds:
+        # play random sound from key's sound list
+        playsound(random.choice(keypress_special_sounds[key.name]), False)
+    else:
+        # else play generic keystroke sound
+        playsound(random.choice(keypress_generic_sounds), False);
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main();
-
